25da57172c98afe17b56c163d0100fb7d3d42342
Romi Kuntsman <romi@totango.com>
2014-08-24 22:45:55 +0300

    Getting and Cleaning Data Course Project

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-GetDataProject
-==============
+---
+title: "Getting and Cleaning Data Course Project"
+author: "Romi Kuntsman"
+date: "08/23/2014"
+output: html_document
+---
+
+# Code Book
+
+1. Subject
+2. Activity
+3. Time Body Acceleration Magnitude Mean
+4. Time Body Acceleration Magnitude StdDev
+5. Time Gravity Acceleration Magnitude Mean
+6. Time Gravity Acceleration Magnitude StdDev
+7. Time Body Acceleration Jerk Magnitude Mean
+8. Time Body Acceleration Jerk Magnitude StdDev
+9. Time Body Gyroscope Magnitude Mean
+10. Time Body Gyroscope Magnitude StdDev
+11. Time Body Gyroscope Jerk Magnitude Mean
+12. Time Body Gyroscope Jerk Magnitude StdDev
+13. Freq Body Acceleration Magnitude Mean
+14. Freq Body Acceleration Magnitude StdDev
+15. Freq Body Body Acceleration Jerk Magnitude Mean
+16. Freq Body Body Acceleration Jerk Magnitude StdDev
+17. Freq Body Body Gyroscope Magnitude Mean
+18. Freq Body Body Gyroscope Magnitude StdDev
+19. Freq Body Body Gyroscope Jerk Magnitude Mean
+20. Freq Body Body Gyroscope Jerk Magnitude StdDev
+
+## Variables
+1: Subject - participant #2 from 1 to 30
+2: Activity during measurement, one of:
+ + LAYING
+ + SITTING
+ + STANDING
+ + WALKING
+ + WALKING_DOWNSTAIRS
+ + WALKING_UPSTAIRS
+3-20: Average of measurements per subject and activity
+
+# Instruction List
+
+1. Load activity labels, subjects, features, measurements and activities
+2. Make the feature names more readable
+3. Filter out only Mean and StdDev featurees
+4. Get activity names instead of id
+5. Merge activity names, subjects and measurements data
+6. Calculate mean, grouped by Subject and Activity
+7. Sort dataset by Subject and Activity
+8. Write result to data_sum.txt
---
+++ run_analysis.R
@@ -0,0 +1,57 @@
+require(plyr)
+require(data.table)
+
+setwd("/home/romi/Education/Documents/Coursera/Data Science/getdata-006/project/UCI HAR Dataset/")
+
+beautifyFeatureName <- function (featureName) {
+  featureName <- sub("\\(\\)$","", featureName)
+  featureName <- sub("-mean$","Mean", featureName)
+  featureName <- sub("-std$","StdDev", featureName)
+  featureName <- sub("^t","Time ", featureName)
+  featureName <- sub("^f","Freq ", featureName)
+  featureName <- sub("BodyBody","Body Body ", featureName)
+  featureName <- sub("Body","Body ", featureName)
+  featureName <- sub("Gravity","Gravity ", featureName)
+  featureName <- sub("Jerk","Jerk ", featureName)
+  featureName <- sub("Acc","Acceleration ", featureName)
+  featureName <- sub("Gyro","Gyroscope ", featureName)
+  featureName <- sub("Mag","Magnitude ", featureName)
+  return(featureName);
+}
+
+# load data
+if (!exists("experiment_dt")) {
+  # read raw data
+  activity_labels = read.table("./activity_labels.txt", col.names = c("activity_id", "activity_label"))
+  subjects = rbind(read.table("./test/subject_test.txt", col.names=c("Subject")),
+                   read.table("./train/subject_train.txt", col.names=c("Subject")))
+  features = read.table("./features.txt", col.names = c("feature_id", "feature_name"))
+  measurements = rbind(read.table("./test/X_test.txt"), read.table("./train/X_train.txt"))  
+  activities = rbind(read.table("./test/y_test.txt", col.names = c("activity_id")),
+                 read.table("./train/y_train.txt", col.names = c("activity_id")))  
+  
+  # make nice feature names and assign to measurement columns
+  features$feature_name_pretty = beautifyFeatureName(features$feature_name)
+  colnames(measurements) <- features$feature_name_pretty
+  
+  # filter just mean and std columns from measurements
+  features_choice = grep(" Mean$| StdDev$", features$feature_name_pretty)
+  feautures_selected = features[features_choice,]
+  measurements_selected_features = measurements[,features_choice]
+  
+  # get activity names instead of id
+  activities_named = subset(join(activities, activity_labels, by="activity_id", type="left", match="all"), select=c("activity_label"))
+  colnames(activities_named) = c("Activity")
+  
+  # bind columns together
+  experiment_dt = data.table(cbind(subjects, activities_named, measurements_selected_features))
+}
+
+# calculate mean, grouped by subject and activity
+data_sum = experiment_dt[, lapply(.SD, mean), by=c("Subject", "Activity")]
+
+# sort final data
+data_sum_sort <- data_sum[order(Subject, Activity),]
+
+# output data
+write.table(data_sum_sort, file="data_sum.txt", row.names = FALSE)
